a832cefc84dc81bf5b50585d68e4a6148283f21f
haavardkjetil <vargo244@gmail.com>
2015-02-04 15:22:22 +0100

    added init bash file

---
+++ init.sh
@@ -0,0 +1,26 @@
+#start with command . init.sh
+
+clear 
+
+echo "Starter smartgit..."
+echo
+cd
+cd sambaad-hhholta/smartgit/bin
+
+xterm -e sh smartgit.sh &
+
+echo "Starter sublime..."
+echo
+cd
+cd sambaad-hhholta/Sublime
+xterm -e ./sublime_text &
+
+echo "setting working directory..."
+echo
+cd
+cd sambaad-hhholta/Exercise4
+
+echo "Initialisering ferdig"
+echo
+
+
