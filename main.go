48a82a8d3fddea568f559173a71879cfeefd1e58
haavardkjetil <vargo244@gmail.com>
2015-02-08 13:45:45 +0100

    lagt til driver files og oppdatert main med to testfunksjoner

--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import(
 "fmt"
 )
 
-func main(){
+func main() {
+	testIO()
+}
+
+
+func testNetwork(){
 	transmitChannel := make(chan network.Packet,5)
 	go network.ReceiveMessage(transmitChannel)
 	for {
@@ -13,4 +18,31 @@ func main(){
 		fmt.Println(message.Type)
 		fmt.Println(message.Postition)
 	}
+}
+
+
+func testIO() {
+	// Initialize hardware
+    if !driver.Init() {
+        log.Fatal("Unable to initialize elevator hardware!\n")
+    }
+
+    println("Press STOP button to stop elevator and exit program.\n")
+
+    driver.SetMotorDirection(-1)
+
+    for {
+        // Change direction when we reach top/bottom floor
+    	if driver.GetFloorSensorSignal() == 3 {
+            driver.SetMotorDirection(-1)
+        } else if driver.GetFloorSensorSignal() == 0 {
+            driver.SetMotorDirection(1)
+        }
+
+        // Stop elevator and exit program if the stop button is pressed
+        if driver.GetStopSignal() {
+        	driver.SetMotorDirection(0)
+            break
+        }
+    }
 }
